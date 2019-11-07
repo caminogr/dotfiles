ffa987a708c78350fc65646984dc1917846aaced
kaminora <kaminoura9207@gmail.com>
2019-11-08 03:57:07 +0900

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for file in .??*
+for dotfile in .??*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.config' ]
     then
         ln -snfv "$PWD/$dotfile" $HOME
     elif [ $dotfile == '.config' ]; then
-        ln -snfv "$PWD/.config/nvim" "$HOME/.config/"
+        ln -snfv "$PWD/.config/nvim" "$HOME/.config"
+        ln -snfv "$PWD/.config/efm-langserver" "$HOME/.config"
     fi
 done
