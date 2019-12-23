8699bd02ce97697dfb02d3512ed4179b94f38466
kaminora <kaminoura9207@gmail.com>
2019-12-24 07:45:17 +0900

    Introduce alacritty

--- .zshrc
+++ .zshrc
@@ -1,3 +1,5 @@
+export LANG=ja_JP.UTF-8
+
 setopt nonomatch
 
 ## prompt
--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ do
     elif [ $dotfile == '.config' ]; then
         ln -snfv "$PWD/.config/nvim" "$HOME/.config"
         ln -snfv "$PWD/.config/efm-langserver" "$HOME/.config"
+        ln -snfv "$PWD/.config/alacritty" "$HOME/.config"
     fi
 done
