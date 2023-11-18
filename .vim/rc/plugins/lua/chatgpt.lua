bea3f14bf5a33aec3da20bf062ac785a9dc221ff
kaminora <kaminoura9207@gmail.com>
2023-11-17 20:47:11 -0500

    update chatgpt model

--- .vim/rc/plugins/lua/chatgpt.lua
+++ .vim/rc/plugins/lua/chatgpt.lua
@@ -91,7 +91,7 @@ require("chatgpt").setup(
       },
     },
     openai_params = {
-      model = "gpt-4",
+      model = "gpt-4-1106-preview",
       frequency_penalty = 0,
       presence_penalty = 0,
       max_tokens = 300,
@@ -100,7 +100,7 @@ require("chatgpt").setup(
       n = 1,
     },
     openai_edit_params = {
-      model = "code-davinci-edit-001",
+      model = "gpt-4-1106-preview",
       temperature = 0,
       top_p = 1,
       n = 1,
